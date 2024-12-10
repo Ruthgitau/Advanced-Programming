58862161677c54b383145d5af10b50f32f1d0b19
Muthoni Gitau <105669933+Ruthgitau@users.noreply.github.com>
2024-12-10 18:51:15 +0300

    assignment 1- question3.java
    Exception handling

---
+++ src/assignment 1- question3.java
@@ -0,0 +1,20 @@
+package Assignment_1;
+
+public class InsufficientFundsException extends Exception {
+    private double amount;
+    private double balance;
+    
+    public InsufficientFundsException(double amount, double balance) {
+        super("Insufficient funds: Required " + amount + ", but only " + balance + " available");
+        this.amount = amount;
+        this.balance = balance;
+    }
+    
+    public double getAmount() {
+        return amount;
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+}
